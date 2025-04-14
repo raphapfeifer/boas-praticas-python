a27dc978e5f80153dd6fdd439bb38cc57a05bd95
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2025-04-13 21:48:50 -0300

    Add tests with products and users

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,43 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"mensagem":"Bem-vindo à API de Recomendação de Produtos"}
+    assert response.json() == {
+        "mensagem": "Bem-vindo à API de Recomendação de Produtos"
+    }
+
+
+def test_criar_produtos():
+    response = client.post(
+        "/produtos/",
+        json={
+            "nome": "Produto A",
+            "categoria": "Categoria 1",
+            "tags": ["tag1", "tag2"],
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()["nome"] == "Produto A"
+
+
+def test_listar_produtos():
+    response = client.get("/produtos/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_criar_usuarios():
+    response = client.post("/usuarios/", params={"nome": "Usuaério Teste"})
+    assert response.status_code == 200
+    usuario_data = response.json()
+    assert usuario_data["id"] == 1
+    assert usuario_data["nome"] == "Usuaério Teste"
+
+
+def test_listar_usuarios():
+    response = client.get("/usuarios/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
